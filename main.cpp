d0af583ccf2aa0c472509eac8a81f6f742525c94
Edgars Sidorovs <126489847+Edgarsid@users.noreply.github.com>
2023-02-27 17:48:12 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "izvelne.h"   // Te glabaajas funkcijas
+#include "structure.h"  // Te ir struktuura
+
+using namespace std;
+
+int main()
+{
+	menu ();  // galvenaa izveelne
+	
+    system("pause");
+    return 0;
+}
